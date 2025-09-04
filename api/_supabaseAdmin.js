fa0895d3371304ca78cc168e72b4d85850cf8d68
fortune14group-hub <fortune14group@gmail.com>
2025-09-04 18:17:20 +0200

    Update _supabaseAdmin.js

--- api/_supabaseAdmin.js
+++ api/_supabaseAdmin.js
@@ -1,1 +1,7 @@
+// api/_supabaseAdmin.js
+import { createClient } from "@supabase/supabase-js";
 
+export const supabaseAdmin = createClient(
+  process.env.SUPABASE_URL,          // samma URL som i appen
+  process.env.SUPABASE_SERVICE_ROLE  // Service Role (hemlig – endast på server)
+);
