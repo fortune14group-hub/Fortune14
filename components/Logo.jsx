fa42f6c61e53c3ef060d44e56f4949c9175faa00
Betspread <fortune14group@gmail.com>
2025-09-26 11:08:20 +0200

    Simplify BetSpread wordmark

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -7,8 +7,8 @@ export default function Logo({ className, priority = false }) {
     <Image
       src="/betspread-logo.svg"
       alt="BetSpread"
-      width={1024}
-      height={576}
+      width={640}
+      height={200}
       className={className}
       sizes="(max-width: 520px) 150px, (max-width: 720px) 170px, (max-width: 900px) 188px, 220px"
       priority={priority}
